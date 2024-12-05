2817a6e0be2f7a7e48da13608daf42abfe39347b
richardr1126 <richardr1126@gmail.com>
2024-12-05 01:18:12 -0700

    More logging for firhose data stream termination

--- start_stream.py
+++ start_stream.py
@@ -3,20 +3,27 @@ import threading
 import signal
 
 from utils import config
+from utils.logger import logger
 import firehose.data_stream as data_stream
 from firehose.data_filter import operations_callback
 
 def main():
     stream_stop_event = threading.Event()
 
-    def sigint_handler(*_):
-        print('Stopping data stream...')
+    def handle_termination(signum, frame):
+        logger.info(f'Received termination signal {signum}. Stopping firehose stream...')
         stream_stop_event.set()
         sys.exit(0)
 
-    signal.signal(signal.SIGINT, sigint_handler)
+    for sig in [signal.SIGINT, signal.SIGTERM, signal.SIGHUP]:
+        signal.signal(sig, handle_termination)
 
-    data_stream.run(config.SERVICE_DID, operations_callback, stream_stop_event)
+    try:
+        data_stream.run(config.SERVICE_DID, operations_callback, stream_stop_event)
+    except Exception as e:
+        logger.error(f"An exception occurred in the firehose: {e}")
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
+
