ae3a5ceb075bd0c38153e04c9dafe482d43fad37
Richard Roberson <richardr1126@gmail.com>
2024-12-15 22:34:38 -0700

    Change logger format

--- firehose/utils/logger.py
+++ firehose/utils/logger.py
@@ -3,6 +3,6 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(
   level=logging.INFO,
-  format='%(asctime)s |%(levelname)s| %(message)s',
+  format='[%(asctime)s] [%(levelname)s] %(message)s',
   datefmt='%H:%M:%S'
 )
